25a23cc206ecb72bdf795eca3c2bde1ed388f86f
June <rlawns3244@naver.com>
2019-04-01 16:04:17 +0900

    Solved Codeforce_550_A

--- CodeForces_550_A.cpp
+++ CodeForces_550_A.cpp
@@ -5,7 +5,7 @@ char charr[100];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int T; string str; string result;
+	int T; string str; string result; bool flag = true;
 	char minval = 127, maxval = -127; int i, sum=0;
 	cin >> T;
 	while (T--) {
@@ -16,17 +16,19 @@ int main() {
 			minval = minval > str[i] ? str[i] : minval;
 			maxval = maxval < str[i] ? str[i] : maxval;
 		}
-		minval -= 97; maxval -= 97;
+		minval -= 'a'; maxval -= 'a';
 		for (i = (int)minval; i <= (int)maxval; i++) {
-			sum += (int)charr[i];
+			if (charr[i] != 1) {
+				flag = false;
+				break;
+			}
 		}
-		if ((int)maxval - (int)minval + 1 == sum) result = "yes";
-		else if((int)maxval - (int)minval + 1 != sum) result = "no";
+		if (flag) result = "yes";
+		else result = "no";
 
 		cout << result << '\n';
-		minval = 127, maxval = -127; sum = 0;
+		minval = 127, maxval = -127; sum = 0; flag = true;
 		for (auto &s : charr) s = 0;
 	}
 	return 0;
-} //Wrong answer on test 3
-
+} //Accepted
